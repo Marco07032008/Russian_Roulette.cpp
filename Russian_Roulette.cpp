507c62b42bdba892036c1b4667fd36b914c5b2bb
Marco07032008 <108406384+Marco07032008@users.noreply.github.com>
2024-01-09 10:06:22 +0100

    Update Russian_Roulette.cpp

--- Russian_Roulette.cpp
+++ Russian_Roulette.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-    srand(time(0));  // Inizializza il generatore di numeri casuali una sola volta
+    srand(time(0));
 
     cout << "Welcome to Russian Roulette" << endl << endl;
     cout << "Press 's' to start" << endl << endl;
@@ -13,81 +13,73 @@ int main() {
     char start;
     cin >> start;
 
-    // Il gioco chiede all'utente che ha avviato la partita se vuole conoscere le regole, y==yes and n==no
     if (start == 's') {
         cout << "Good, the game is going to start" << endl << endl;
     }
 
-    // Inizializzazione delle vite
-    int Vita = 2;   // Vite del giocatore
-    int VitaD = 2;  // Vite del dealer
+    int Vita = 2;   // Player's lives
+    int VitaD = 2;  // Dealer's lives
 
-    // Numero iniziale di proiettili veri e blank
     int ProiettiliVeri = 2;
     int ProiettiliBlank = 2;
 
-    // Richiesta del nome dell'utente fuori dal ciclo while
     cout << "What's your name?" << endl << endl;
     string name;
     cin >> name;
 
-    // Ciclo principale del gioco
-    while (Vita > 0 && VitaD > 0 && ProiettiliVeri > 0) {
+    while (Vita > 0 && VitaD > 0 && (ProiettiliVeri + ProiettiliBlank) > 0) {
         cout << "Hi " << name << ", welcome to the Russian Roulette" << endl << endl;
         cout << "This is the first round, there is " << ProiettiliVeri << " real bullet(s) and " << ProiettiliBlank << " blank(s)" << endl << endl;
 
-        // Probabilità del 50% per il proiettile vero e 50% per il proiettile blank
-        int randomNum = rand() % 2;  // Genera un numero casuale tra 0 e 1
-        // Moltiplica per il totale dei proiettili per ottenere un numero compreso tra 0 e il totale effettivo dei proiettili
-        randomNum *= (ProiettiliVeri + ProiettiliBlank);
+        int randomNum = rand() % (ProiettiliVeri + ProiettiliBlank);
 
         cout << "It's your turn, press 'Y' to shoot at yourself and 'D' to shoot at the dealer" << endl << endl;
         char shoot1;
         cin >> shoot1;
 
         if (randomNum < ProiettiliVeri) {
-            // Il proiettile è vero solo se premi 'Y'
             if (shoot1 == 'Y') {
                 cout << "Bang! You lost a life!" << endl << endl;
                 Vita--;
-                ProiettiliVeri--;  // Un proiettile vero è stato sparato
+                ProiettiliVeri--;
             } else {
                 cout << "Click! The gun is blank.";
                 if (ProiettiliBlank > 0) {
                     ProiettiliBlank--;
-                    // Verifica se il dealer può sparare in risposta
-                    int dealerShoot = rand() % 2;
-                    if (dealerShoot < ProiettiliVeri) {
-                        cout << " Dealer shot back! You lost a life!" << endl << endl;
-                        Vita--;
+
+                    // Reload if both real and blank bullets are at 0
+                    if (ProiettiliVeri == 0 && ProiettiliBlank == 0) {
+                        cout << " Reloading... You now have 3 real bullets and 2 blanks." << endl << endl;
+                        ProiettiliVeri = 3;
+                        ProiettiliBlank = 2;
                     } else {
-                        cout << " Dealer's shot is blank. You're lucky!" << endl << endl;
+                        // Dealer shoots back
+                        int dealerShoot = rand() % (ProiettiliVeri + ProiettiliBlank);
+                        if (dealerShoot < ProiettiliVeri) {
+                            cout << " Dealer shot back! You lost a life!" << endl << endl;
+                            Vita--;
+                        } else {
+                            cout << " Dealer's shot is blank. You're lucky!" << endl << endl;
+                        }
                     }
                 } else {
                     cout << " You're safe!" << endl << endl;
                 }
             }
         } else {
-            // Il proiettile è sempre blank quando premi 'Y'
             if (shoot1 == 'Y') {
                 cout << "Click! The gun is blank. You're safe!" << endl << endl;
-                // Rimetti il proiettile blank nel caricatore
                 if (ProiettiliBlank > 0) {
                     ProiettiliBlank--;
                 }
             } else if (shoot1 == 'D') {
-                // Ora gestiamo la probabilità per il dealer
-                int randomNumDealer = rand() % 2;  // Genera un numero casuale tra 0 e 1
-                // Moltiplica per il totale dei proiettili per ottenere un numero compreso tra 0 e il totale effettivo dei proiettili
-                randomNumDealer *= (ProiettiliVeri + ProiettiliBlank);
-
-                if (randomNumDealer < ProiettiliVeri) {
+                int dealerShoot = rand() % (ProiettiliVeri + ProiettiliBlank);
+                if (dealerShoot < ProiettiliVeri) {
                     cout << "Bang! The dealer lost a life!" << endl << endl;
                     VitaD--;
-                    ProiettiliVeri--;  // Un proiettile vero è stato sparato
+                    ProiettiliVeri--;
                 } else {
                     cout << "Click! The gun is blank. Dealer's lucky!" << endl << endl;
-                    // Rimetti il proiettile blank nel caricatore e riduci il numero di proiettili blank
                     if (ProiettiliBlank > 0) {
                         ProiettiliBlank--;
                     }
@@ -95,13 +87,11 @@ int main() {
             }
         }
 
-        // Stampa delle vite rimanenti
         cout << "Your remaining lives: " << Vita << endl;
         cout << "Dealer's remaining lives: " << VitaD << endl;
         cout << "Remaining bullets: " << ProiettiliVeri + ProiettiliBlank << " (Real: " << ProiettiliVeri << ", Blank: " << ProiettiliBlank << ")" << endl << endl;
     }
 
-    // Verifica chi ha vinto
     if (Vita > 0) {
         cout << "Congratulations! You won!" << endl;
     } else if (ProiettiliVeri == 0) {
